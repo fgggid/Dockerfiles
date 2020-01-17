a379411f974c676ec979b58e1b6be52500aa0720
fgggid <alf.redyoung@gmail.com>
2020-01-17 17:21:44 +0800

    reset IP6, in case current vif does not have IPv6 address, it will use previous one

--- my-utils/get-ll-addr.sh
+++ my-utils/get-ll-addr.sh
@@ -5,7 +5,7 @@ FILTER_OUT_NH="$(rt --dump 0 | egrep "^169\.254\.0\.0" | awk '{print $5}')"
 rt --dump 0 | egrep "^169\.254\." | awk -v NH="$FILTER_OUT_NH" '{ \
     if ($5 != NH) { \
         split($1, f, "/"); \
-        HOST=f[1]; \
+        HOST=f[1]; IP6=""; \
         "nh --get " $5 " | grep Oif | awk \"{print \\$2}\" | cut -d: -f2" | getline OIF; \
         "vif -g " OIF " | grep IPaddr | awk \"{print \\$3}\" | cut -d: -f2-" | getline IP4; \
         "vif -g " OIF " | grep IP6addr | awk \"{print \\$1}\" | cut -d: -f2-" | getline IP6; \
