44e523112a6a85ab80b8ce31685fb7e7bea2c470
fgggid <alf.redyoung@gmail.com>
2020-02-10 23:11:00 +0800

    add NIC info for get-ll-addr.sh

--- my-utils/get-ll-addr.sh
+++ my-utils/get-ll-addr.sh
@@ -9,10 +9,11 @@ rt --dump 0 | egrep "^169\.254\." | awk -v NH="$FILTER_OUT_NH" '{ \
         "nh --get " $5 " | grep Oif | awk \"{print \\$2}\" | cut -d: -f2" | getline OIF; \
         "vif -g " OIF " | grep IPaddr | awk \"{print \\$3}\" | cut -d: -f2-" | getline IP4; \
         "vif -g " OIF " | grep IP6addr | awk \"{print \\$1}\" | cut -d: -f2-" | getline IP6; \
+        "vif -g " OIF " | grep ^vif | awk \"{print \\$3}\"" | getline DEV; \
         if (length(IP6) == 0) { \
-            print HOST " V4:" IP4; \
+            print HOST " NIC:" DEV " V4:" IP4; \
         } else { \
-            print HOST " V6:" IP6; \
+            print HOST " NIC:" DEV " V6:" IP6; \
         } \
     } \
 }'
