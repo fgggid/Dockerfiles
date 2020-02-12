f62ed56671d454b14b3cbbb9caa880fc249324b3
fgggid <alf.redyoung@gmail.com>
2020-02-12 15:20:16 +0800

    fix error when no VM

--- my-utils/get-ll-addr.sh
+++ my-utils/get-ll-addr.sh
@@ -8,6 +8,9 @@ echo "$LINES" |
 while IFS= read -r line
 do
     NH=`echo $line | awk '{print $5}'`
+    if [ "x$NH" = "x" ]; then
+        continue
+    fi
     HOST=`echo $line | cut -d/ -f1`
     IP6=""
     OIF=`nh --get $NH | grep Oif | awk '{print $2}' | cut -d: -f2`
@@ -17,7 +20,7 @@ do
     DEV=`echo "$OIF_INFO" | grep ^vif | awk '{print $3}'`
     VIF=`echo "$OIF_INFO" | grep ^vif | awk '{print $1}' | cut -d/ -f2`
     VRF=`echo "$OIF_INFO" | grep Vrf | awk '{print $1}' | cut -d: -f2`
-    if [ x"$IP6" = "x" ]; then
+    if [ "x$IP6" = "x" ]; then
         printf "%-15s NIC:%s V4:%-39s\tVRF:%s VIF:%s\n" $HOST $DEV $IP4 $VRF $VIF
     else
         printf "%-15s NIC:%s V6:%-39s\tVRF:%s VIF:%s\n" $HOST $DEV $IP6 $VRF $VIF
