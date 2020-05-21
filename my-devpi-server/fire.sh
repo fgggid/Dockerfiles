12bd02bd74bc392582e92e4a5962edfddac4dcb9
fgggid <alf.redyoung@gmail.com>
2020-05-21 23:55:37 +0800

    add docker-compose support

--- my-devpi-server/fire.sh
+++ my-devpi-server/fire.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker run -d --name devpi-server \
-    -e DEVPI_PASSWORD=huayun \
-    -e DEVPI_HOST=10.130.176.10 \
-    -v "${PWD}/data":/data \
-    -p "3141:3141" \
-    --restart always \
-    devpi-server:latest --web --role replica --master-url http://178.104.163.176:3141
+#docker run -d --name devpi-server \
+#    -e DEVPI_PASSWORD=huayun \
+#    -e DEVPI_HOST=178.104.163.176 \
+#    -v /root/arsdn_repo/devpi:/data \
+#    -p "3141:3141" \
+#    --restart always \
+#    devpi-server:latest --web --role master
+
+docker-compose up -d
