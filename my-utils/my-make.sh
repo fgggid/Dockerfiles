ce0509e15bc0ca6111aa32d87e20505cdb1c11ba
fgggid <alf.redyoung@gmail.com>
2021-01-12 14:59:00 +0800

    remove time which break return status

--- my-utils/my-make.sh
+++ my-utils/my-make.sh
@@ -15,7 +15,7 @@ TARGET=$1
 shift
 {
     exec 3>&1    # Save the current stdout (the cat pipe)
-    time make -O ${TARGET} $@ SRCVER=${MY_VER} BUILDTAG=${MY_VER} 2>&1 1>&3 | tee /tmp/err.log
+    make -O ${TARGET} $@ SRCVER=${MY_VER} BUILDTAG=${MY_VER} 2>&1 1>&3 | tee /tmp/err.log
 } | cat > temp/${TARGET}.log
 set +x
 
